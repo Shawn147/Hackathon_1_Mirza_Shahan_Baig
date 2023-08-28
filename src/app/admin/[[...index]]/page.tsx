1bf4557c61c6da18f8b1d099d8fc8c833e683c9f
Shawn147 <baigshahan@gmail.com>
2023-08-28 23:16:42 +0500

    Update page.tsx

--- src/app/admin/[[...index]]/page.tsx
+++ src/app/admin/[[...index]]/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { NextStudio } from "next-sanity/studio";
-import config from "@/sanity.config";
+import sanityConfig from "../../../../sanity.config";
+
 export default function AdminPage() {
-  return <NextStudio config={config} />;
+  return <NextStudio config={sanityConfig} />;
 }
